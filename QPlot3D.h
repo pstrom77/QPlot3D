d93cc4e15ae769b65b86592a8a67bea64ca09846
Peter Strömbäck <peter.stromback@yahoo.se>
2013-12-31 14:21:12 +0100

    Removed old contact info

--- QPlot3D.h
+++ QPlot3D.h
@@ -2,7 +2,6 @@
  **
 ** Copyright (C) 2013 Peter Strömbäck <peter.stromback@yahoo.se>
 **
-** Contact: http://www.qt-project.org/legal			       
 ** This program is free software: you can redistribute it and/or modify
 ** it under the terms of the GNU General Public License as published by
 ** the Free Software Foundation, either version 3 of the License, or
